12381b744c3c239de3b4f6ef71072b8e62eab209
sadhna <sadhna@bittern.co>
2018-08-13 13:15:07 +0530

    chat

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root 'conversations#index'
+
+resources :conversations, only: [:index, :show]
+resources :users, only: [:index]
+resources :personal_messages, only: [:new, :create]
+mount ActionCable.server => '/cable'
+end
